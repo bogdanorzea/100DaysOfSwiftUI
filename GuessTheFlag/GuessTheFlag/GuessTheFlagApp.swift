3b8ee0c17ce7e7d676c36fd868725e3fd1515863
Bogdan Orzea <bogdan.orzea@gmail.com>
2022-05-05 21:48:48 -0400

    Completes GuessTheFlag project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Bogdan Orzea on 2022-05-02.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
