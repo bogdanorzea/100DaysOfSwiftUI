8654784a564540d3c2be4e6702d098c8365b7e64
Bogdan Orzea <bogdan.orzea@gmail.com>
2022-05-05 21:44:23 -0400

    Completes Checkpoint1

---
+++ Checkpoint1.playground/Contents.swift
@@ -0,0 +1,6 @@
+// https://www.hackingwithswift.com/quick-start/beginners/checkpoint-1
+
+let celsius: Double = 34.0
+let fahrenheit = celsius *  9 / 5 + 32
+
+print("Temperature in \(celsius)℃ or \(fahrenheit)℉")
