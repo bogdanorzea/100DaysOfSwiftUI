83057039d653093f5d8b70d69c9387d97c854385
Bogdan Orzea <bogdan.orzea@gmail.com>
2022-05-05 21:44:49 -0400

    Completes Checkpoint2

---
+++ Checkpoint2.playground/Contents.swift
@@ -0,0 +1,7 @@
+// https://www.hackingwithswift.com/quick-start/beginners/checkpoint-2
+
+let albums = ["Fearless", "Red", "1986", "Confessions", "1986"]
+let uniqueAlbums = Set(albums)
+
+print("Numbers of albums is \(albums.count)")
+print("Numbers of uniques albums is \(uniqueAlbums.count)")
