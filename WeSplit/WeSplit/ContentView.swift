a37d701bb1a5ccc8b7a44d020977ec3b209fda2e
Bogdan Orzea <bogdan.orzea@gmail.com>
2022-05-05 21:48:24 -0400

    Completes WeSplit project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+
+struct ContentView: View {
+    private let currencyCode = Locale.current.currencyCode ?? "USD"
+    
+    @State private var numberOfPeople: Int = 2
+    @State private var checkAmount: Double = 100.0
+    @State private var tipPercentage: Int = 20
+    private let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let finalAmount = checkAmount * (1 + Double(tipPercentage) / 100)
+        
+        return finalAmount / peopleCount
+    }
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: currencyCode))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: currencyCode))
+                } header: {
+                    Text("Amount per person")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Bogdan Orzea on 2022-04-30.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
