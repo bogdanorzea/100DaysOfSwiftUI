484947eb64cd03c822130e1f472ba87f2c13232f
Bogdan Orzea <bogdan.orzea@gmail.com>
2023-05-05 21:19:37 -0400

    Completes Project 17

---
+++ Flashzilla/EditCards.swift
@@ -0,0 +1,87 @@
+//
+//  EditCards.swift
+//  Flashzilla
+//
+//  Created by Bogdan Orzea on 2023-05-05.
+//
+
+import SwiftUI
+
+struct EditCards: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var cards = [Card]()
+    @State private var newPrompt = ""
+    @State private var newAnswer = ""
+    
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section("Add new card") {
+                    TextField("Prompt", text: $newPrompt)
+                    TextField("Answer", text: $newAnswer)
+                    Button("Add Card", action: addCard)
+                }
+                
+                Section {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        VStack(alignment: .leading) {
+                            Text(cards[index].prompt)
+                                .font(.headline)
+                            
+                            Text(cards[index].answer)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .onDelete(perform: removeCard)
+                }
+            }
+            .navigationTitle("Edit Cards")
+            .toolbar {
+                Button("Done", action: done)
+            }
+            .listStyle(.grouped)
+            .onAppear(perform: loadData)
+        }
+    }
+    
+    func done() {
+        dismiss()
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
+            }
+        }
+    }
+    
+    func saveData() {
+        if let data = try? JSONEncoder().encode(cards) {
+            UserDefaults.standard.set(data, forKey: "cards")
+        }
+    }
+    
+    func addCard() {
+        let trimmedPrompt = newPrompt.trimmingCharacters(in: .whitespaces)
+        let trimmedAnswer = newAnswer.trimmingCharacters(in: .whitespaces)
+        guard trimmedAnswer.isEmpty || trimmedPrompt.isEmpty else { return }
+        
+        let card = Card(prompt: trimmedPrompt, answer: trimmedAnswer)
+        cards.insert(card, at: 0)
+        saveData()
+    }
+    
+    func removeCard(at index: IndexSet) {
+        cards.remove(atOffsets: index)
+        saveData()
+    }
+}
+
+struct EditCards_Previews: PreviewProvider {
+    static var previews: some View {
+        EditCards()
+    }
+}
---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Bogdan Orzea on 2023-04-08.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
